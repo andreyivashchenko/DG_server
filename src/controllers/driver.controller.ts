fec34b39464082cd0d3045b3d6cc96ccea1f04a2
Pavel Pavlov <ppavvvlov@gmail.com>
2024-04-13 09:51:45 +0300

    work

--- src/controllers/driver.controller.ts
+++ src/controllers/driver.controller.ts
@@ -61,5 +61,35 @@ class DriverController {
             res.status(500).json({message: `DB error`, err: err});
         }
     }
+    async getDrivers(
+        req: Request,
+        res: Response<{
+            success: boolean;
+            message: string;
+            data?: {driver_id: number; object_group_id: number; full_name: string; name_org: string; status: string}[];
+        }>
+    ) {
+        try {
+            const drivers = await pool.query(
+                'select driver_id, object_group_id, full_name, name_org, status from drivers '
+            );
+            if (!drivers.rows) {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Drivers not found'
+                });
+            }
+            res.status(200).json({
+                success: true,
+                message: 'ok!',
+                data: drivers.rows
+            });
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: 'DB error'
+            });
+        }
+    }
 }
 export default new DriverController();
--- src/routes/driver.routes.ts
+++ src/routes/driver.routes.ts
@@ -3,7 +3,8 @@ import DriverController from '../controllers/driver.controller';
 
 const router = express.Router();
 
-router.post('/', DriverController.updateCoordinates);
+router.post('/coordinates', DriverController.updateCoordinates);
+router.get('/', DriverController.getDrivers);
 router.get('/:driver_id', DriverController.getDriverPosition);
 
 export default router;
